5ba55206ec3b2c08f33b90c0c8fb14b94526a4d1
Eben Mallory <ebenryanmallory@gmail.com>
2019-02-11 22:01:20 -0700

    created basic route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Router, Link } from "@reach/router";
 
 class App extends Component {
   render() {
     return (
-      <div>hello world!</div>
+      <div>
+        <nav>
+          <Link to="/">Home</Link>{" "}
+          <Link to="dashboard">City</Link>
+        </nav>
+    
+        <Router>
+          <Home path="/" />
+          <Dashboard path="/dashboard" />
+        </Router>
+      </div>
     );
   }
 }
+
+const Home = () => (
+  <div>
+    <h2>Hello</h2>
+  </div>
+);
+
+const Dashboard = () => (
+  <div>
+    <h2>Future search city</h2>
+  </div>
+);
 
 export default App;
